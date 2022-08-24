6c5fdec0959d266459f3814ec79339a5e27fb522
WillianGust <williandes.silva@yahoo.ca>
2022-08-23 23:35:06 -0400

    Carregamento em cascata AJAX dropdown para outro

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/estados', to: 'estados#index'
   get '/cidades', to: 'estados#lista_cidades'
+
+  root to: 'home#index'
+  # root to: 'home'
 end
