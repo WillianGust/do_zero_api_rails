d9db500dc4ba1dd4e801bce456ab5e12ba34d6a5
WillianGust <williandes.silva@yahoo.ca>
2022-09-06 21:11:38 -0400

    Construindo tabela com post ajax

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
   before_action :set_car, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /cars or /cars.json
   def index
